fb91a3f8035df6b34cb8307edeb19cc19d123fcb
Stefano L <stefanosemail@protonmail.com>
2022-09-17 07:19:32 +0100

    hsekpng

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3003",
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // }
+    baseUrl: 'http://localhost:3003'
   }
 })
