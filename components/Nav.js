4544466fd0ec31fc79664be8e743f4d57ddc9b53
Stefano L <stefanosemail@protonmail.com>
2022-09-18 02:12:51 +0100

    hskepng, nprocess update

--- components/Nav.js
+++ components/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
 
       <Link href="/sale">Sale</Link>
 
-      <Link href="/orders">Orders</Link>
+      <Link href="/signin">Orders</Link>
 
       <Link href="/account">Account</Link>
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,14 @@
-import Document, { Html, Head, NextScript, Main } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document, { Html, Head, NextScript, Main } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
-    const sheet = new ServerStyleSheet();
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />)
-    );
-    const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(
+      App => props => sheet.collectStyles(<App {...props} />)
+    )
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
   }
 
   render() {
@@ -16,10 +16,10 @@ export default class MyDocument extends Document {
       <Html lang="en-CA">
         <Head />
         <body>
-        <Main />
-        <NextScript />
+          <Main />
+          <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
