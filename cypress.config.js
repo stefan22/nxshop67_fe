4f4dd8d8c65d11116dfeb8a80020d4899a8e86cd
Stefano L <stefanosemail@protonmail.com>
2022-09-28 05:16:16 +0100

    test

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   projectId: 'uap4i3',
   e2e: {
-    baseUrl: process.env.FRONTEND_URL_DEV,
-    defaultCommandTimeout: 10000,
+    // baseUrl: process.env.FRONTEND_URL_DEV,
+    specPattern:'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
+
   }
 })
