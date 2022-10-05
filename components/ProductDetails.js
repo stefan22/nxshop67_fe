7bef5a3ee7ff4b4c4e94d588d04dffd88dbb090e
Stefano L <stefanosemail@protonmail.com>
2022-10-05 19:58:37 +0100

    prdetails update

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import Image from 'next/image'
 
 const ProductDetails = ({ product }) => {
   return (
-    <>
-      <Image
-        src={product?.photo.image.publicUrlTransformed}
-        alt={product?.name}
-        width={640}
-        height={725}
-      />
+    <div data-testid="product-details">
+      <picture>
+        <img
+          src={product?.photo.image.publicUrlTransformed}
+          alt={product?.name}
+          width={640}
+          height={725}
+        />
+      </picture>
       <h2>{product?.name}</h2>
       <p>{product?.price}</p>
       <p>{product?.description}</p>
-    </>
+    </div>
   )
 }
 
