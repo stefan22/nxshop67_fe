f81caa981702913b174de14d277ab5cb30967eb2
Stefano L <stefanosemail@protonmail.com>
2022-10-05 23:50:23 +0100

    userQuery

---
+++ graphql/currentUserQuery.js
@@ -0,0 +1,30 @@
+import { gql, useQuery } from '@apollo/client'
+
+const currentUserQuery = gql`
+  query {
+    authenticatedItem {
+      ... on User {
+        id
+        email
+        name
+      }
+    }
+  }
+`
+
+const useCurrentUser = () => {
+  const {
+    data: currentUser,
+    loading: userLoading,
+    error: userError
+  } = useQuery(currentUserQuery)
+
+  if (userLoading) return <p>Loading...</p>
+  if (userError) return console.log(userError)
+
+  return {
+    currentUser: currentUser?.authenticatedItem
+  }
+}
+
+export { useCurrentUser }
