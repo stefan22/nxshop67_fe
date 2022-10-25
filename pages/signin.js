d402eb1650fcc7159e9eb88795aaa0ebb80dae96
stefan22 <stefanosemail@protonmail.com>
2022-10-25 09:46:57 +0100

    nw wkflow and refactr signinup comp

--- pages/signin.js
+++ pages/signin.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import Router from 'next/router'
-import Link from 'next/link'
-import SigninSl from '../components/styles/SigninSl'
+import SignInUp from '@/components/SignInUp'
 import useForm from '../lib/useForm'
 import { gql } from 'graphql-tag'
 import { useMutation } from '@apollo/client'
@@ -25,7 +24,7 @@ const signinMutation = gql`
   }
 `
 
-const SignIn = () => {
+const SignInPage = () => {
   const [error, setError] = useState({ message: '' })
   const { input, handleChange, resetForm } = useForm({
     email: '',
@@ -54,65 +53,17 @@ const SignIn = () => {
   }
 
   return (
-    <SigninSl>
-      <div className="login">
-        <header>
-          <h1>Signin</h1>
-          <p>Welcome back!</p>
-        </header>
-        <p className="error-message">{error?.message}</p>
-
-        <div className="login__body">
-          <form method="POST" onSubmit={handleSubmit}>
-            <fieldset>
-              <section className="input-field">
-                <label htmlFor="email">
-                  Email:
-                  <input
-                    type="email"
-                    placeholder="Email"
-                    autoComplete="current-email"
-                    required
-                    name="email"
-                    value={input.email}
-                    onChange={handleChange}
-                  />
-                </label>
-              </section>
-
-              <section className="input-field">
-                <label htmlFor="password">
-                  Password:
-                  <span>Password Reset</span>
-                  <input
-                    type="password"
-                    placeholder="Password"
-                    autoComplete="current-password"
-                    name="password"
-                    value={input.password}
-                    onChange={handleChange}
-                  />
-                </label>
-              </section>
-
-              <div className="buttons-group">
-                <section className="submit-button">
-                  <button type="submit">Sign in</button>
-                </section>
-
-                <section className="login__no-account">
-                  <p>Don&apos;t have an account?</p>
-                  <Link href="/signup">
-                    <button type="button">Sign up</button>
-                  </Link>
-                </section>
-              </div>
-            </fieldset>
-          </form>
-        </div>
-      </div>
-    </SigninSl>
+    <SignInUp
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+      heading={'Welcome back!'}
+      error={error}
+      inputs={input}
+      hrefLink={'/signup'}
+      btnLabel={'Sign up'}
+      btnIntroMsg={"Don't have an account?"}
+    />
   )
 }
 
-export default SignIn
+export default SignInPage
