d9ea1b595b27b10fc7fdfde4e2eedd3d55028c57
Stefano L <stefanosemail@protonmail.com>
2023-03-20 02:18:44 +0000

    pass reset p1

--- frontend/features/form/index.js
+++ frontend/features/form/index.js
@@ -1,5 +1,5 @@
 import useForm from './useForm'
 import signInMutation from './signInMutation'
-import signUpMutation from "./signUpMutation";
+import signUpMutation from './signUpMutation'
 
 export { useForm, signInMutation, signUpMutation }
