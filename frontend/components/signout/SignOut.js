db29e349d4991f4a268905a41f3085904bd8ccb9
stefan22 <stefanosemail@proton.me>
2023-04-21 02:29:27 +0100

    fixed end session/comp unmount

--- frontend/components/signout/SignOut.js
+++ frontend/components/signout/SignOut.js
@@ -1,24 +1,22 @@
 import { useMutation } from '@apollo/client'
 import { signOutMutation } from '../../features/form'
 import { useRouter } from 'next/router'
-import { currentUserQuery, useCurrentUser } from '../../features/current-user'
+import { currentUserQuery } from '../../features/current-user'
 
 const SignOut = () => {
   const router = useRouter()
-  const { user } = useCurrentUser()
   const [signout] = useMutation(signOutMutation, {
     refetchQueries: [{ query: currentUserQuery }]
   })
 
   const doSignout = async e => {
     e.preventDefault()
+    const { data } = await signout()
     // eslint-disable-next-line no-console
-    console.log('user signout.. ', user)
+    console.log('user signout.. ', data)
     // end session
-    await signout()
-    return router.push('/')
+    if (data?.endSession) return router.push('/')
   }
-
   return (
     <button type="button" onClick={doSignout}>
       Sign Out
