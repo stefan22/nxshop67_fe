96f48e84e5c540a5ab047d1f294c3ef4f19c18cd
stefan22 <stefanosemail@proton.me>
2023-04-21 21:57:10 +0100

    gen amends

--- frontend/pages/_error.js
+++ frontend/pages/_error.js
@@ -12,6 +12,7 @@ const Error = ({ statusCode }) => {
 
 Error.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+
   return { statusCode }
 }
 
