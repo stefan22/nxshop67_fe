e6602fc7b328cbb82b772a1e2adeacb66564552b
stefan22 <stefanosemail@proton.me>
2023-04-21 02:41:03 +0100

    gen amends

---
+++ frontend/utils/useFetch.js
@@ -0,0 +1,38 @@
+/*
+ *  @fn useFetch
+ * @param  It takes a Url/ endpoint parameter
+ * @returns data, loading and error <object>
+ * */
+
+import { useState, useEffect } from 'react'
+
+const useFetch = url => {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url)
+        const jsonData = await response.json()
+        setData(jsonData)
+        setLoading(false)
+      } catch (err) {
+        setError(err)
+        setLoading(false)
+        // eslint-disable-next-line no-console
+        console.log(`Failed to fetch. Error: ${err}`)
+      }
+    }
+    fetchData()
+  }, [url])
+
+  return {
+    data,
+    loading,
+    error
+  }
+}
+
+export default useFetch
