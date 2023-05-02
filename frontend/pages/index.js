aa225d54948992d8ced97911faa8208617f60ab6
stefan22 <stefanosemail@proton.me>
2023-05-02 21:20:45 +0100

    package downshift

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,19 @@
 import Carousel from '../components/carousel'
 import { SkeleLoader, withLoading } from '../components/loader'
+import Search from '../components/search'
+
+const Home = () => {
+  return (
+    <>
+      <Carousel />
+      <Search />
+    </>
+  )
+}
 
 export async function getServerSideProps({ req }) {
   const headers = req ? req.headers : {}
   return { props: { headers } }
 }
 
-export default withLoading(Carousel, SkeleLoader)
+export default withLoading(Home, SkeleLoader)
