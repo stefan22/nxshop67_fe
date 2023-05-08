85ee907e432a7209cd5e6c797e26aa3687732841
stefan22 <stefanosemail@proton.me>
2023-05-08 23:45:44 +0100

    account pg n updte checkout

--- frontend/pages/account/Account.js
+++ frontend/pages/account/Account.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
 import { useCurrentUser } from '../../features/current-user'
 import { capitalize } from '../../utils'
+import { Items } from './Items'
+import {
+  AccountContainer,
+  AccountGroup,
+  AccountBtn,
+  AccountBottom,
+  H1
+} from './Account.styled'
 import { GrMoney } from 'react-icons/gr'
 
-const Account = () => {
+const Account = props => {
   const { user } = useCurrentUser()
   let username
 
@@ -15,22 +25,44 @@ const Account = () => {
       <Head>
         <title>NXShop67 - Account:{username}</title>
       </Head>
-      <h1>
+      <H1>
         Account{' '}
         <GrMoney
           id="gmoney"
           size={24}
           style={{
             stroke: 'red',
-            strokeWiidth: '1px',
+            strokeWidth: '1px',
             fillRule: 'evenodd',
             fill: 'grey'
           }}
         />{' '}
         Central
-      </h1>
+      </H1>
 
-      <h3> Welcome {username}</h3>
+      <h4> Welcome {username}</h4>
+      <AccountContainer>
+        {Items.map(itm => (
+          <AccountGroup key={itm.id}>
+            <Image
+              width={640}
+              height={284}
+              layout="intrinsic"
+              objectFit="cover"
+              loading="lazy"
+              src={itm.imageUrl}
+              alt={itm.title}
+            />
+            <AccountBottom>
+              <h3>{itm.heading}</h3>
+              <p>{itm.description}</p>
+              <AccountBtn>
+                <Link href={itm.linkTo}>{itm.button}</Link>
+              </AccountBtn>
+            </AccountBottom>
+          </AccountGroup>
+        ))}
+      </AccountContainer>
     </>
   )
 }
